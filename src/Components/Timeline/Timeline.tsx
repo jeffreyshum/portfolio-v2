ad4694ccd1111e9c5885f6f9c5d137eebc2c4a6b
jeffreyshum <89279966+jeffreyshum@users.noreply.github.com>
2022-08-05 14:17:40 -0400

    Added Font Color Separation

--- src/Components/Timeline/Timeline.tsx
+++ src/Components/Timeline/Timeline.tsx
@@ -6,22 +6,24 @@ import bxsciImage from "../../images/bronx-science.png"
 const Timeline: FC = () => {
 	return (
 		<section id={styles.timelineContainer}>
-			<h1>Timeline</h1>
+			<h1>Curriculum Vitae</h1>
 			<div className={styles.barContainer}>
 				<div className={styles.bar}></div>
 			</div>
 			{/* Bronx Science */}
 			<div className={styles.item}>
 				<div>
-					<h1 className={styles.time}>2016 - 2021</h1>
+					<h1 className={styles.time} data-past>
+						2016 - 2021
+					</h1>
 					<h2 className={styles.title}>
 						The Bronx High School of Science
 					</h2>
 					<h3 className={styles.description}>Secondary Education</h3>
 					<p className={styles.summary}>
-						One of the nine prestigious specialized high schools in
-						New York City, with eight Nobel prize winning alumni
-						(the most of any high school). The cirriculum puts great
+						One of nine prestigious specialized high schools in New
+						York City, with eight Nobel prize winning alumni (the
+						most of any high school). The cirriculum puts great
 						emphasis on the sciences, mathematics, and social
 						issues.
 					</p>
@@ -49,9 +51,43 @@ const Timeline: FC = () => {
 				</h3>
 				<p className={styles.summary}></p>
 				<p className={styles.awards}>
-					Prospective Bachelors Degree
+					Prospective Bachelor's Degree
 					<br />
 					Grade Point Average: 3.97
+				</p>
+			</div>
+			{/* Teaching Assistant */}
+			<div className={styles.item}>
+				<h1 className={styles.time}>2022 - Current</h1>
+				<h2 className={styles.title}>
+					Undergraduate Teaching Assistant
+				</h2>
+				<h3 className={styles.description}>
+					at Stony Brook University's Department of Applied
+					Mathematics & Statistics
+				</h3>
+				<p className={styles.summary}>
+					{" "}
+					<p className={styles.awards}>
+						Assist teaching material, grading assignments, and
+						tutoring undergraduate students in{" "}
+						<span className={styles.focusText}>
+							AMS 103 -{" "}
+							<em>Applied Mathematics in Modern Technology</em>
+						</span>{" "}
+						under the guidance and mentorship of Professor Matthew
+						G. Reuter of Stony Brook University's Department of
+						Applied Mathematics & Statistics.
+						<br />
+						<br />
+						<b>
+							<em>Class Topics Include</em>
+						</b>
+						: Algebra, Systems of Equations, Coordinate Systems,
+						Base-n (including Binary), Modular Arithmetic,
+						Probability, Encryption, Error-Correcting Codes, Graphs,
+						Algorithms, Big-O Notation
+					</p>
 				</p>
 			</div>
 		</section>
